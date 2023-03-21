48cecfd3bef26eb390fcb4cce087028a5bca2b58
sergo <sergo.ngn@gmail.com>
2023-03-21 14:46:36 +0200

    cleanup

--- keys.js
+++ keys.js
@@ -4,10 +4,4 @@ module.exports = {
   pgHost: process.env.PG_HOST,
   pgDB: process.env.PG_DB,
   pgPort: process.env.PG_PORT,
-
-  //   pgUser: "postgres",
-  //   pgPass: "postgres",
-  //   pgHost: "localhost",
-  //   pgDB: "postgres",
-  //   pgPort: 5432,
 };
