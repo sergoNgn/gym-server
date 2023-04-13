e3d4c464f396ae10478dcbffeb4f4e54d154f1c6
sergo <sergo.ngn@gmail.com>
2023-04-13 18:45:30 +0300

    add exercies post

--- index.js
+++ index.js
@@ -91,3 +91,19 @@ app.get("/categories", async (req, res) => {
 
   res.send(values.rows);
 });
+
+app.post("/exercies", async (req, res) => {
+  const query = {
+    name: "save",
+    text: "INSERT INTO exercises (name, category_id, sex, client_id) values ($1, $2, $3, $4) RETURNING id",
+    values: [
+      req.body.name,
+      req.body.categoryId,
+      req.body.isMen,
+      req.body.clientId,
+    ],
+  };
+  const saved = await db.pgClient.query(query);
+
+  res.status(200).send(saved.rows[0]);
+});
